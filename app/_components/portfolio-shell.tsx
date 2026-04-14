8713ea4eef20ce8ca0f17629544873c7f46d0eba
locust08 <locust08@users.noreply.github.com>
2026-04-15 07:32:23 +0800

    Load legacy portfolio runtime directly

--- app/_components/portfolio-shell.tsx
+++ app/_components/portfolio-shell.tsx
@@ -1,4 +1,3 @@
-import Script from "next/script";
 import { FireplaceAudioController } from "./fireplace-audio-controller";
 import { HomeIntroBypassController } from "./home-intro-bypass-controller";
 import { HomeTextOverridesController } from "./home-text-overrides-controller";
@@ -14,11 +13,31 @@ export function PortfolioShell() {
       <ProjectGalleryVideoController />
       <ProjectTextOverridesController />
       <div id="root" suppressHydrationWarning />
-      <Script
-        src="/assets/index-BKlZLtrc.js"
-        type="module"
-        strategy="afterInteractive"
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function () {
+  var host = window.location.hostname;
+  var params = new URLSearchParams(window.location.search);
+  var shouldUseSafeMode = (host === "localhost" || host === "127.0.0.1") && params.get("webgl") !== "1";
+  if (!shouldUseSafeMode) return;
+  function patchReadPixels(Ctor) {
+    if (!Ctor || !Ctor.prototype || Ctor.prototype.__portfolioReadPixelsPatched) {
+      return;
+    }
+    var originalReadPixels = Ctor.prototype.readPixels;
+    Ctor.prototype.readPixels = function () {
+      return;
+    };
+    Ctor.prototype.__portfolioReadPixelsPatched = true;
+    Ctor.prototype.__portfolioOriginalReadPixels = originalReadPixels;
+  }
+  patchReadPixels(window.WebGLRenderingContext);
+  patchReadPixels(window.WebGL2RenderingContext);
+  window.__portfolioWebglSafeMode = true;
+})();`,
+        }}
       />
+      <script src="/assets/index-BKlZLtrc.js" type="module" />
     </>
   );
 }
